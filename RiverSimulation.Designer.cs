af9db7ea7d76d166852a3503af151a0b85fb73a7
Asion Lin <asion.lin@gmail.com>
2014-06-10 00:17:28 +0800

    First UI
    建立專案與入口畫面，Release版本建置於Exe目錄，Debug版本於原本的bin/Debug目錄。

---
+++ RiverSimulation.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class RiverSimulation
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.檔案FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新增專案NToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開起專案OToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.儲存專案SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存專案為AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.結束XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.說明HToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.關於AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.語系切換LToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.繁體中文TToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.檔案FToolStripMenuItem,
+            this.語系切換LToolStripMenuItem,
+            this.說明HToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(808, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 檔案FToolStripMenuItem
+            // 
+            this.檔案FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新增專案NToolStripMenuItem,
+            this.開起專案OToolStripMenuItem,
+            this.儲存專案SToolStripMenuItem,
+            this.另存專案為AToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.結束XToolStripMenuItem});
+            this.檔案FToolStripMenuItem.Name = "檔案FToolStripMenuItem";
+            this.檔案FToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.檔案FToolStripMenuItem.Text = "檔案(F)";
+            // 
+            // 新增專案NToolStripMenuItem
+            // 
+            this.新增專案NToolStripMenuItem.Name = "新增專案NToolStripMenuItem";
+            this.新增專案NToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.新增專案NToolStripMenuItem.Text = "新增專案(N)";
+            // 
+            // 開起專案OToolStripMenuItem
+            // 
+            this.開起專案OToolStripMenuItem.Name = "開起專案OToolStripMenuItem";
+            this.開起專案OToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.開起專案OToolStripMenuItem.Text = "開起專案(O)";
+            // 
+            // 儲存專案SToolStripMenuItem
+            // 
+            this.儲存專案SToolStripMenuItem.Name = "儲存專案SToolStripMenuItem";
+            this.儲存專案SToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.儲存專案SToolStripMenuItem.Text = "儲存專案(S)";
+            // 
+            // 另存專案為AToolStripMenuItem
+            // 
+            this.另存專案為AToolStripMenuItem.Name = "另存專案為AToolStripMenuItem";
+            this.另存專案為AToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.另存專案為AToolStripMenuItem.Text = "另存專案為(A)...";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(158, 6);
+            // 
+            // 結束XToolStripMenuItem
+            // 
+            this.結束XToolStripMenuItem.Name = "結束XToolStripMenuItem";
+            this.結束XToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.結束XToolStripMenuItem.Text = "結束(X)";
+            // 
+            // 說明HToolStripMenuItem
+            // 
+            this.說明HToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.關於AToolStripMenuItem});
+            this.說明HToolStripMenuItem.Name = "說明HToolStripMenuItem";
+            this.說明HToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.說明HToolStripMenuItem.Text = "說明(H)";
+            // 
+            // 關於AToolStripMenuItem
+            // 
+            this.關於AToolStripMenuItem.Name = "關於AToolStripMenuItem";
+            this.關於AToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.關於AToolStripMenuItem.Text = "關於(A)";
+            // 
+            // 語系切換LToolStripMenuItem
+            // 
+            this.語系切換LToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eToolStripMenuItem,
+            this.繁體中文TToolStripMenuItem});
+            this.語系切換LToolStripMenuItem.Name = "語系切換LToolStripMenuItem";
+            this.語系切換LToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.語系切換LToolStripMenuItem.Text = "語系切換(L)";
+            // 
+            // eToolStripMenuItem
+            // 
+            this.eToolStripMenuItem.Name = "eToolStripMenuItem";
+            this.eToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.eToolStripMenuItem.Text = "English(E)";
+            this.eToolStripMenuItem.Click += new System.EventHandler(this.eToolStripMenuItem_Click);
+            // 
+            // 繁體中文TToolStripMenuItem
+            // 
+            this.繁體中文TToolStripMenuItem.Name = "繁體中文TToolStripMenuItem";
+            this.繁體中文TToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.繁體中文TToolStripMenuItem.Text = "繁體中文(T)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "匯入";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(354, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "模擬模組";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(229, 294);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 64);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "水理";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(479, 294);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 64);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "動床";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(229, 396);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 64);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "初始條件";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(479, 396);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(102, 64);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "邊界條件";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(354, 498);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(102, 64);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "開始模擬";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(354, 600);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(102, 64);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "模擬結果展示";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // RiverSimulation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 682);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "RiverSimulation";
+            this.Text = "水理動床模式使用者介面";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 檔案FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新增專案NToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開起專案OToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 儲存專案SToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 另存專案為AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 結束XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 語系切換LToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 說明HToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 關於AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 繁體中文TToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+

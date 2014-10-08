6670f8bdaa39dd5ebb1eafa2044c9f572c3c52a7
alcor0 <asionlin@ms35.url.com.tw>
2014-10-09 00:41:28 +0800

    Add sample for read input file

---
+++ Samples/TestGoogleMap/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TestGoogleMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1013, 551);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 576);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Samples/TestGoogleMap/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGoogleMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        CoorPoint[,] inputCoor;
+        double maxX = double.MinValue, minX = double.MinValue;
+        double maxY = double.MinValue, minY = double.MinValue;
+        private bool ReadInputFile(string path)
+        {
+            const int MaxLineWord = 3;
+            try
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(path);
+                char[] charSeparators = new char[] { '\t', ' ' };
+
+                string line = file.ReadLine();
+                string[] words = line.Split(charSeparators, MaxLineWord);
+                int j_count = Convert.ToInt32(words[0]);
+                int i_count = Convert.ToInt32(words[1]);
+
+                inputCoor = new CoorPoint[i_count, j_count];
+                int i = 0, j = j_count - 1;
+                while ((line = file.ReadLine()) != null)
+                {
+                    words = line.Split(charSeparators, MaxLineWord);
+                    inputCoor[i, j] = new CoorPoint(Convert.ToDouble(words[0]), Convert.ToDouble(words[1]));
+                    if (inputCoor[i, j].x > maxX)
+                        maxX = inputCoor[i, j].x;
+                    if (inputCoor[i, j].x < minX)
+                        minX = inputCoor[i, j].x;
+                    if (inputCoor[i, j].y > maxY)
+                        maxY = inputCoor[i, j].y;
+                    if (inputCoor[i, j].y < minY)
+                        minY = inputCoor[i, j].y;
+
+                    if (--j < 0)
+                    {
+                        j = j_count - 1;
+                        ++i;
+                    }
+                    if (i == i_count)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (ReadInputFile(Environment.CurrentDirectory + "\\cchemesh.geo"))
+            {
+                MessageBox.Show("Finished!");
+            }
+        }
+    }
+    public class CoorPoint
+    {
+        public CoorPoint()
+	    {
+            _x = 0.0;
+            _y = 0.0;
+	    }
+
+        public CoorPoint(double x1, double y1)
+        {
+            _x = x1;
+            _y = y1;
+        }
+
+        private double _x, _y;
+        public double x
+        {
+            //set the person name
+            set { this._x = value; }
+            //get the person name 
+            get { return this._x; }
+        }
+        public double y
+        {
+            //set the person name
+            set { this._y = value; }
+            //get the person name 
+            get { return this._y; }
+        }
+    }
+}
